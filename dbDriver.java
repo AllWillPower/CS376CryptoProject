61452fde11ba61743839bb92017f8418aa460068
JordanAllen08 <allenjt6055@uwec.edu>
2021-04-19 10:44:59 -0500

    Add files via upload
    This required javadb.jar Request access on discord server!

---
+++ dbDriver.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.sql.*;
+import java.util.Properties;
+
+
+
+public class dbDriver {
+
+    public static void main(String[] args) throws SQLException {
+        Connection dbConnection = null;
+        //base connection to database
+        try {
+            String url = "jdbc:mysql://35.202.111.69:3306/spamFilterData";
+            Properties info = new Properties();
+            info.put("user", "root");
+            info.put("password", "CSUWEC");
+            dbConnection = DriverManager.getConnection(url, info);
+
+            if (dbConnection != null) {
+                System.out.println("Successfully connected to MySQL database test");
+            } } catch (SQLException ex) {
+            System.out.println("An error occurred while connecting MySQL databse");
+            ex.printStackTrace(); }
+
+        //first query set
+            Statement state = dbConnection.createStatement();
+            ResultSet rs;
+        //query command
+            rs = state.executeQuery("SELECT * FROM learnData;");
+            while ( rs.next() ) {
+                String id = rs.getString("id");
+                String data = rs.getString("message");
+                String flag = rs.getString("flag");
+
+                System.out.print(id + ": " + flag + ": " + data + "\n");
+            }
+            dbConnection.close();
+
+
+
+        }
+    }
+
+
+
+
